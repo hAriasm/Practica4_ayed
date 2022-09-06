352c1992d4c42d1f6d9face4f7f01e8b1e3cf35a
Eder Ampuero <eampuero@unsa.edu.pe>
2022-09-05 23:53:56 -0500

    prueba 4

--- KNN/config.js
+++ KNN/config.js
@@ -4,10 +4,10 @@ datasets:
 [
     {
         label: "Rain data chart",
-        data: trainingSet.map((point)=> {
+        data: trainingSet().map((point)=> {
             return {x:point.humidity, y:point.pressure}
             } ) ,
-        pointBackgroundColor: trainingSet.map((point)=> {
+        pointBackgroundColor: trainingSet().map((point)=> {
             return point.rain ? 'red' : 'blue' 
             } ) , 
         pointRadius: 5.5 ,
@@ -16,7 +16,60 @@ datasets:
     }
                         
 ]
-
 }
+}
+function charOptions() {
+return {
+        maintainAspectRatio: false ,
+        legend:
+        {
+           labels: 
+           {
+            fontSize: 20
+           }     
+        },
+        responsive: true,
+        scales:
+        {
+            xAxes:
+            [
+                {
+                    display: true ,
+                    scaleLabel:
+                    {
+                        display: true,
+                        labelString: 'Humidity' ,
+                        fontSize: 20
+                    } ,
+                    ticks:
+                    {
+                        fontSize: 20,
+                        max: 10,
+                        min: 0
+                    }
+                }
+                
+            ],
+            yAxes: 
+            [
+                {
+                    display: true ,
+                    scaleLabel:
+                    {
+                        display: true,
+                        labelString: 'Pressure' ,
+                        fontSize: 20
+                    } ,
+                    ticks:
+                    {
+                        fontSize: 20,
+                        max: 10,
+                        min: 0
+                    }
+                }
+                
+            ]
 
+        }
+}
 }
