42c2668d251b48c1e44244a88a79f0ad669ca522
Eder Ampuero <eampuero@unsa.edu.pe>
2022-09-06 00:14:09 -0500

    prueba5_knn

--- KNN/script.js
+++ KNN/script.js
@@ -3,3 +3,31 @@ type: 'scatter',
 data: CharacterData() , 
 options: charOptions()
 })
+
+document.querySelector('#plot').addEventListener('click', ()=> {
+chart.data.datasets[0].data.push({
+x: Number(document.querySelector('#humidity').value) ,
+y: Number(document.querySelector('#pressure').value)
+}
+)
+document.querySelector('#predict').addEventListener('click', () =>{
+    const x = chart.data.datasets[0].data[chart.data.datasets[0].data.length-1].x 
+    const y = chart.data.datasets[0].data[chart.data.datasets[0].data.length-1].y
+    let distances = []
+
+    trainingSet().forEach((point)=> {
+
+       // https://www.youtube.com/watch?v=dT4SrxoQS-4  -----link referencia
+
+
+    })
+
+
+)
+
+
+}
+
+
+
+)
