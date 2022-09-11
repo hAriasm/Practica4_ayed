772528762c6fa38b400ddedfac650d361f5a2700
Edison Pérez <edisonaqp@gmail.com>
2022-09-11 17:07:34 -0500

    query range draw

--- kdtree/sketch-rq-rect.js
+++ kdtree/sketch-rq-rect.js
@@ -21,16 +21,6 @@ function setup() {
   createCanvas();
   createCanvas(width, height);
 
-  background(0);
-  for (var x = 0; x < width; x += width / scalex) {
-    for (var y = 0; y < height; y += height / scaley) {
-      stroke(125, 125, 125);
-      strokeWeight(1);
-      line(x, 0, x, height);
-      line(0, y, width, y);
-    }
-  }
-
   for (let i = 0; i < 150; i++) {
     var x = Math.floor(Math.random() * maxx);
     var y = Math.floor(Math.random() * maxy);
@@ -51,33 +41,40 @@ function drawPoint(point, r = 255, g = 255, b = 255) {
   text(x + ", " + y, (x * width) / maxx + 5, height - (y * height) / maxy - 5);
 }
 
-
-function draw() {
-
+function showData() {
   background(0);
-  noFill();
-  stroke(0, 255, 0);
-  strokeWeight(4);
-  rectMode(CENTER);
-  let range = new Rectangle(mouseX, mouseY, 100, 100);
-  rect(range.x, range.y, range.w * 2, range.h * 2);
-
-
-
   stroke('blue');
   strokeWeight(1);
+
+  // line guides
+  // for (var x = 0; x < width; x += width / scalex) {
+  //   for (var y = 0; y < height; y += height / scaley) {
+  //     stroke(125, 125, 125);
+  //     strokeWeight(0.2);
+  //     line(x, 0, x, height);
+  //     line(0, y, width, y);
+  //   }
+  // }
 
   for (let i = 0; i < data.length; i++) {
     x = data[i][0];
     y = data[i][1];
     fill(255, 255, 255, 200);
     circle(x * width / maxx, height - y * height / maxy, 10);
-    // textSize(12);
-    // text(
-    //   x + ", " + y,
-    //   (x * width) / maxx + 5,
-    //   height - (y * height) / maxy - 5
-    // );
   }
+}
+
+function draw() {
+
+  background(0);
+
+  showData();
+
+  noFill();
+  stroke(0, 255, 0);
+  strokeWeight(4);
+  rectMode(CENTER);
+  let range = new Rectangle(mouseX, mouseY, 100, 100);
+  rect(range.x, range.y, range.w * 2, range.h * 2);
 
 }
